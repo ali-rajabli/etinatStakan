956a2626ab23472f5a0dd5becd313fae9da05f14
ali-rajabli <ali.recebli9@gmail.com>
2021-05-22 12:37:03 +0400

    bugs fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,18 +62,17 @@ setInterval(function() {
     if (index == images_array.length - 1) index = 0;
 }, 5000);
 
-// Səbətin PopUp kimi açılması
+// Səbətin hover olanda açılması
 
-$('#cart-button').on('click', function() {
-    $('#basket-pop-up').css('display', 'block');
-});
+$('#cart-button').hover(function() {
 
-$('.close').on('click', function() {
-    $('#basket-pop-up').css('display', 'none');
+}, function() {
+
 })
 
-$(window).on('click', function(event) {
-    if ($('#basket-pop-up').is(event.target)) {
-        $('#basket-pop-up').css('display', 'none');
-    }
+$('#cart-button').mouseover(function() {
+    $('.minicart').css('display', "block")
+})
+$('.cart-items').mouseleave(function() {
+    $('.minicart').css('display', "none")
 })
